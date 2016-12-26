aaf88158585fd243c9dab58930712ed79640123e
Jose <me@josearodriguez.me>
2016-12-26 21:01:37 +0100

    Metodos para realizar la resta Se han aÃadido los metodos para la realizaciÃn de la resta

--- src/egc/Resta.java
+++ src/egc/Resta.java
@@ -1,5 +1,10 @@
 package egc;
-
+import java.lang.Math;
 public class Resta {
 
+	public static int restar(int x, int z){
+		
+		return Math.abs(x-z);
+	}
+	
 }
--- test/egc/TestResta.java
+++ test/egc/TestResta.java
@@ -1,5 +1,25 @@
 package egc;
 
-public class TestResta {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class TestResta {
+	@Test
+	public void testResta() {
+		assertEquals(2, Resta.restar(2, 4));
+	}
+
+	@Test
+	public void testRestaCero() {
+		assertEquals(4, Resta.restar(0, 4));
+		assertEquals(4, Resta.restar(4, 0));
+
+	}
+	
+	@Test
+	public void testRestaNegativo(){
+		assertEquals(-4, Resta.restar(0, -4));
+	}
+	
 }
