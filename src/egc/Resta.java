6c397fe62ce46196b42f4fa2b4aa7adf39266564
Jose <me@josearodriguez.me>
2016-12-26 21:50:13 +0100

    variable para producir conflicto

--- src/egc/Resta.java
+++ src/egc/Resta.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 public class Resta {
 
 	private int restarParaQueDeConflicto2(int a, int b,int c){
-		
+		String conflicto ="esta variable debe de dar conflicto a la larga..."
 		return Math.abs(a-b);
 	}
 	public static int restar(int... numeros) {
